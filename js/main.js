88da4a87947aaf559b760d3d8c7e5d34f3873859
kaylaaraujo <arau0014@algonquinlive.com>
2014-11-25 17:51:28 -0500

    removed arrow code...I don't have arrows

--- js/main.js
+++ js/main.js
@@ -21,22 +21,17 @@ var dropdownArray = Array.prototype.slice.call(dropdown,0);
 
 dropdownArray.forEach(function(el){
 	var button = el.querySelector('a[data-toggle="dropdown"]'),
-			menu = el.querySelector('.dropdown-menu'),
-			arrow = button.querySelector('i.icon-arrow');
+			menu = el.querySelector('.dropdown-menu');
 
 	button.onclick = function(event) {
 		if(!menu.hasClass('show')) {
 			menu.classList.add('show');
 			menu.classList.remove('hide');
-			arrow.classList.add('open');
-			arrow.classList.remove('close');
 			event.preventDefault();
 		}
 		else {
 			menu.classList.remove('show');
 			menu.classList.add('hide');
-			arrow.classList.remove('open');
-			arrow.classList.add('close');
 			event.preventDefault();
 		}
 	};
