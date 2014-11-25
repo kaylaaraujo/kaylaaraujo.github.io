e4d5492c51238c64bedb4230ff8947413bfbc4b9
kaylaaraujo <arau0014@algonquinlive.com>
2014-11-25 16:57:50 -0500

    added css for index1.html

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 var navTop = document.querySelector('.nav-top');
+var dropdown = document.querySelectorAll('.dropdown');
+var dropdownArray = Array.prototype.slice.call(dropdown,0);
 
 	
 	document.querySelector('.nav-btn').addEventListener('click', function(e){
@@ -15,3 +17,31 @@ var navTop = document.querySelector('.nav-top');
 	    this.setAttribute('data-state', 'active');
 		}
 	});
+	
+
+dropdownArray.forEach(function(el){
+	var button = el.querySelector('a[data-toggle="dropdown"]'),
+			menu = el.querySelector('.dropdown-menu'),
+			arrow = button.querySelector('i.icon-arrow');
+
+	button.onclick = function(event) {
+		if(!menu.hasClass('show')) {
+			menu.classList.add('show');
+			menu.classList.remove('hide');
+			arrow.classList.add('open');
+			arrow.classList.remove('close');
+			event.preventDefault();
+		}
+		else {
+			menu.classList.remove('show');
+			menu.classList.add('hide');
+			arrow.classList.remove('open');
+			arrow.classList.add('close');
+			event.preventDefault();
+		}
+	};
+})
+
+Element.prototype.hasClass = function(className) {
+    return this.className && new RegExp("(^|\\s)" + className + "(\\s|$)").test(this.className);
+};
