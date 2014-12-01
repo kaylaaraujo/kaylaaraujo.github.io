741b0b064a6290d050b77a1a6ec04f3329f9fc5f
kaylaaraujo <arau0014@algonquinlive.com>
2014-12-01 14:45:55 -0500

    removed drop down js

--- js/main.js
+++ js/main.js
@@ -18,31 +18,3 @@ var dropdownArray = Array.prototype.slice.call(dropdown,0);
 		}
 	});
 	
-
-dropdownArray.forEach(function(el){
-	var button = el.querySelector('a[data-toggle="dropdown"]'),
-		menu = el.querySelector('.dropdown-menu');
-dropdownArray.forEach(function(el){
-	var menu = el.querySelector('.dropdown-menu');
-		menu.classList.remove('hide');
-		menu.classList.add('show');
-});
-
-	button.onclick = function(event) {
-		if(!menu.hasClass('show')) {
-			menu.classList.add('show');
-			menu.classList.remove('hide');
-			event.preventDefault();
-		}
-		else {
-			menu.classList.remove('show');
-			menu.classList.add('hide');
-			event.preventDefault();
-		}
-	};
-
-})
-
-Element.prototype.hasClass = function(className) {
-    return this.className && new RegExp("(^|\\s)" + className + "(\\s|$)").test(this.className);
-};
